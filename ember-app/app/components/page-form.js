ce6414ee3961b4025523dbd92b88271eebab6c6b
Andrzej Piątyszek <konto.andrzeja@gmail.com>
2016-05-14 16:07:00 +0200

    disable open url button if url is invalid

--- ember-app/app/components/page-form.js
+++ ember-app/app/components/page-form.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
+import Validator from 'npm:validator';
 
 export default Ember.Component.extend({
+  didRender() {
+    this.send('isUrlValid');
+  },
+  actions: {
+    isUrlValid() {
+      this.set('isUrlValid', Validator.isURL(this.get('page.url')));
+    }
+  }
 });
