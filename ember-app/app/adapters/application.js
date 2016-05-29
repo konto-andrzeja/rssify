24fa38d4c6ea96ef170eaedafe349df45223f8c0
Andrzej Piątyszek <konto.andrzeja@gmail.com>
2016-05-29 15:40:00 +0200

    handle sending csrf token back to Rails with every request

--- ember-app/app/adapters/application.js
+++ ember-app/app/adapters/application.js
@@ -1,5 +1,11 @@
 import ActiveModelAdapter from 'active-model-adapter';
 
 export default ActiveModelAdapter.extend({
-  namespace: 'api/v1'
+  namespace: 'api/v1',
+  headers: Ember.computed(function() {
+    var token = document.cookie.match(/X\-CSRF\-TOKEN\=([^;]*)/)[1];
+    return {
+      'X-CSRF-TOKEN': decodeURIComponent(token)
+    };
+  }).volatile()
 });
