16df95af22af32e6679811e2f2c92d14ed28dc6e
Andrzej Piątyszek <konto.andrzeja@gmail.com>
2016-06-04 09:44:00 +0200

    redirect to pages.new route after deleting page

--- ember-app/app/components/pages-list.js
+++ ember-app/app/components/pages-list.js
@@ -2,10 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    deletePage(page) {
-      if(confirm('"' + page.get('name') + '" will be deleted permanently. Are you sure?')) {
-        page.destroyRecord();
-      }
+    onDeleteClick(page) {
+      this.sendAction('deletePage', page);
     }
   }
 });
--- ember-app/app/routes/pages.js
+++ ember-app/app/routes/pages.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('page');
+  },
+  actions: {
+    deletePage(page) {
+      if(confirm('"' + page.get('name') + '" will be deleted permanently. Are you sure?')) {
+        page.destroyRecord().then(() => this.transitionTo('pages.new'));
+      }
+    }
   }
 });
